e718a831818a33f98c29b7a46e6e196555b8767e
betahxy <55125653+betahxy@users.noreply.github.com>
2023-04-17 03:50:19 -0400

    add boot params

--- arch/x86/vmxbooter/kmain.c
+++ arch/x86/vmxbooter/kmain.c
@@ -1,3 +1,9 @@
+#include <asm/bootparam.h>
+
+struct boot_params boot_params;
+char *cmd_line_str = "auto";
+struct setup_data init_setup_data;
+
 void kmain(void);
 extern char input_data, input_data_end;
 /*
@@ -14,14 +20,41 @@ extern char input_data, input_data_end;
 0x1ff       ' 0x1fe
 */
 
+#define IMAGE_VADDR_START (0xffffffff80000000UL)
+#define _va(x) ((unsigned long)(x) + IMAGE_VADDR_START)
+#define _pa(x) ((unsigned long)(x) - IMAGE_VADDR_START)
+
+static __attribute__((unused))
+void *memset(void *dst, int b,  unsigned long len)
+{
+	char *p = dst;
+
+	while (len--)
+		*(p++) = b;
+	return dst;
+}
+
+static __attribute__((unused))
+int memcmp(const void *s1, const void *s2, unsigned long n)
+{
+	size_t ofs = 0;
+	int c1 = 0;
+
+	while (ofs < n && !(c1 = ((unsigned char *)s1)[ofs] - ((unsigned char *)s2)[ofs])) {
+		ofs++;
+	}
+	return c1;
+}
+
 void kmain(void)
 {
 
-	char *vga_base = (char *)(0xb8000 + 0xffffffff80000000);
-	char *vmlinux_base = (char *)0xffffffff81000000;
+	char *vga_base = (char *)_va(0xb8000);
+	char *vmlinux_base = (char *)_va(0x1000000);
 	char *guest_pos = &input_data;
 	char *guest_pos_end = &input_data_end;
 
+	memset(&boot_params, 0, sizeof(boot_params));
 	if ((unsigned long)guest_pos_end > (unsigned long)vmlinux_base) 
 	{
 		while (1)
@@ -38,8 +71,26 @@ void kmain(void)
 	}
 
 	vga_base[0] = '!';
-
-	asm volatile("movabs $0x1000000, %%rax; jmpq   *%%rax;"::);
+	boot_params.sentinel = 0xff;
+	boot_params.hdr.version = 0x0215;
+	boot_params.hdr.vid_mode = 0xFFFF;
+	boot_params.hdr.boot_flag = 0xAA55;
+	((char *)(&boot_params.hdr.header))[0] = 'H';
+	((char *)(&boot_params.hdr.header))[1] = 'd';
+	((char *)(&boot_params.hdr.header))[2] = 'r';
+	((char *)(&boot_params.hdr.header))[3] = 's';
+	boot_params.hdr.type_of_loader = 0xFF;
+	boot_params.hdr.loadflags = 1; // the protected-mode code is loaded at 0x100000
+	boot_params.hdr.ramdisk_image = 0; //The 32-bit linear address of the initial ramdisk or ramfs. Leave at zero if there is no initial ramdisk/ramfs.
+	boot_params.hdr.ramdisk_size = 0; // Size of the initial ramdisk or ramfs. Leave at zero if there is no initial ramdisk/ramfs.
+	boot_params.hdr.heap_end_ptr = 0xe000 - 0x200;
+	boot_params.hdr.loadflags |= 0x80; /* CAN_USE_HEAP */;
+	boot_params.hdr.cmdline_size = _pa(cmd_line_str);
+	boot_params.hdr.cmdline_size = 255;
+	// init_setup_data.next = 0;
+	// boot_params.hdr.setup_data = _pa(&init_setup_data);
+	boot_params.hdr.setup_data = 0; //no setup data
+	asm volatile("mov %0, %%rsi;movabs $0x1000000, %%rax; jmpq   *%%rax;"::"r"((unsigned long)_pa(&boot_params)));
 	while (1)
 	{
 		/* code */
